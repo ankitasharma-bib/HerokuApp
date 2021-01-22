296d7c63391bfd8f22818a8ef6fe5e1cc8fa186f
ankitasharma-bib <ankita.sharma@michelin.com>
2021-01-22 12:10:19 +0530

    Added exp ID attribute

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
     <meta name="salesforce-save-access-token" content="true">
     <meta name="salesforce-forgot-password-enabled" content="<?php echo getenv('SALESFORCE_FORGOT_PASSWORD_ENABLED');?>">
     <meta name="salesforce-self-register-enabled" content="<?php echo getenv('SALESFORCE_SELF_REGISTER_ENABLED');?>">
+	<meta name="salesforce-expid" content="<?php echo getenv('EXP_ID');?>">
     <meta name="salesforce-login-handler" content="onLogin">
     <meta name="salesforce-logout-handler" content="onLogout">
 	<meta name="salesforce-cache-max-age" content="60">
